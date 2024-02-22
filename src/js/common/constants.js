f2a8982075e980d6a047f2ec120e07e9d1f76ed4
Ramsnes <115258511+Ramsnes@users.noreply.github.com>
2024-02-22 10:23:23 +0100

    changed form names, rdy to change to listing submission

--- src/js/common/constants.js
+++ src/js/common/constants.js
@@ -4,3 +4,4 @@ export const BASE_API_URL = "https://api.noroff.dev/api/v1";
 export const REGISTER_API_URL = `${BASE_API_URL}/auction/auth/register`;
 export const LOGIN_API_URL = `${BASE_API_URL}/auction/auth/login`;
 export const POSTS_API_URL = `${BASE_API_URL}/auction/posts`;
+export const CREDITS_API_URL = `${BASE_API_URL}/auction/profiles/{name}/credits`;
---
+++ src/js/credits.js
@@ -0,0 +1,33 @@
+import { fetcher } from "../js/fetcher.js";
+import { getFromLocalStorage } from "../js/common/utils/localStorageUtil.js";
+import { BASE_API_URL } from "./common/constants.js";
+
+document.addEventListener("DOMContentLoaded", async () => {
+  const displayCredit = async () => {
+    try {
+      // Fetches username from local storage
+      const username = getFromLocalStorage("username");
+
+      if (!username) {
+        console.error("Username not available");
+        return;
+      }
+
+      // Fetch credit
+      const creditsResponse = await fetcher(
+        `${BASE_API_URL}/auction/profiles/${username}/credits`,
+        { method: "GET" },
+        true
+      );
+
+      // Display credit
+      const totalCreditElement = document.getElementById("totalCredit");
+      totalCreditElement.innerText = `Total Credit: ${creditsResponse.credits}`;
+    } catch (error) {
+      console.error("Error fetching credit:", error.message);
+    }
+  };
+
+  // Call the function
+  await displayCredit();
+});
